30c7afabdc10ecc0948f570c005c19769060e9a6
Ravina <raniravina71996@gmail.com>
2024-01-27 16:16:29 +0200

    Assignment1 completed

---
+++ Assignment1.py
@@ -0,0 +1,5 @@
+square_meters = float(input("Enter the area of the house in square meters: "))
+
+square_feet = square_meters * 10.764
+
+print("The area of the house is", square_feet, "square feet.")
